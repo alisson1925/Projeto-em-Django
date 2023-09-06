252f1e8499e50bb315a808fd2988aac57686a0c2
Alisson Rodrigues <alisson.r1925@gmail.com>
2023-09-06 13:17:11 -0300

    Views

--- projeto_cad_usuarios/projeto_cad_usuarios/urls.py
+++ projeto_cad_usuarios/projeto_cad_usuarios/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     # rota, view responsável, nome de referência
-    path('', views.home,name='home')
+    path('', views.home,name='home'),
+    # usuarios.com/usuarios
+    path('usuarios/', views.usuarios,name='listagem_usuarios')
 ]
