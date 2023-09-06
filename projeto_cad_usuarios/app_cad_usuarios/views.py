5861914d553f42a60eacdc63bffc41101a16a0f9
Alisson Rodrigues <alisson.r1925@gmail.com>
2023-09-06 14:00:17 -0300

    resolvido

--- projeto_cad_usuarios/app_cad_usuarios/views.py
+++ projeto_cad_usuarios/app_cad_usuarios/views.py
@@ -8,7 +8,7 @@ def usuarios(request):
     # Salvar os dados da tela para o banco de dados.
     novo_usuario = Usuario()
     novo_usuario.nome = request.POST.get('nome')
-    novo_usuario.idade = request.POT.get('idade')
+    novo_usuario.idade = request.POST.get('idade')
     novo_usuario.save()
     
     # Exibir todos os usuários já cadastrados em uma nova página
